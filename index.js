1a321babbf2d1ee73002ac0d6d7dc106888fb324
gx0c <toolong972@gmail.com>
2021-09-26 02:10:39 +0800

    Fixed ident

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const { Client } = require('ssh2');
 
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const client = new Client();
 const prefix = '$';
 
-const client = new Client();
 bot.login(TOKEN);
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
   client.on('ready', () => {
-		console.log('Ssh is ready');
+    console.log('Ssh is ready');
   }).connect({
     host: '127.0.0.1',
     port: 8022,
@@ -22,12 +22,12 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (message) => {
-	if (!message.content.startsWith(prefix))
-		return;
-
-	const command = message.content.substring(prefix.length, message.content.length);
+  if (!message.content.startsWith(prefix))
+    return;
+  
+  const command = message.content.substring(prefix.length, message.content.length);
   client.exec(command, (err, stream) => {
-  	if (err) throw err;
+    if (err) throw err;
     stream.on('data', (data) => {
       console.log('STDOUT: \n' + data);
       message.reply('\n' + data);
